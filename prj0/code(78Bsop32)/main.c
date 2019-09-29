1fb93ffcf7c6c6778d151de3206d7707431f6e00
syYang <yangsyun@gmail.com>
2019-09-29 11:12:10 +0800

    c51 for 内部不能定义局部变量

--- prj0/code(78Bsop32)/main.c
+++ prj0/code(78Bsop32)/main.c
@@ -4,7 +4,6 @@
 void main() {
   SysInit();
   VarsInit();
-
   while (1) {
     F_turnOnWDT();
     // user coding
@@ -16,8 +15,9 @@ void main() {
 }
 //=============================================================================
 void delayMs(uint16_t msCount) {
-  for (uint16_t i = 0; i < msCount; i++) {
-    for (uint16_t j = 0; j < 1000; j++) {
+  uint16_t i, j;
+  for (i = 0; i < msCount; i++) {
+    for (j = 0; j < 1000; j++) {
       /* code */
     }
   }
