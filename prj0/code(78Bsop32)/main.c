4578f1ef5ee6964dc8c8c23d1a3a0ded86ef3617
syYang <yangsyun@gmail.com>
2019-10-11 11:32:58 +0800

    todo：按键每按一次 led 取反显示

--- prj0/code(78Bsop32)/main.c
+++ prj0/code(78Bsop32)/main.c
@@ -4,13 +4,21 @@
 void main() {
   SysInit();
   VarsInit();
+  F_ledOn();
+  delayMs(500);
   while (1) {
-    F_turnOnWDT();
+    //喂狗
+    // F_turnOnWDT();
     // user coding
-    F_ledOff();
-    delayMs(500);
-    F_ledOn();
-    delayMs(500);
+    // F_ledOff();
+    // delayMs(500);
+    // F_ledOn();
+    // delayMs(500);
+    GetKeys();
+    if (D_keyValue1 == keyValue) {
+      F_ledNeg();
+    }
+    keyValue = D_keyNull;
   }
 }
 //=============================================================================
@@ -19,6 +27,7 @@ void delayMs(uint16_t msCount) {
   for (i = 0; i < msCount; i++) {
     for (j = 0; j < 1000; j++) {
       /* code */
+      // F_turnOnWDT();
     }
   }
 }
