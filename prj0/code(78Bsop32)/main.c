5c92e1acc16c79bf5c76db6d03e3008b86a374da
syYang <yangsyun@gmail.com>
2019-10-18 16:04:18 +0800

    看门狗开启； 看门狗清除； 获得按键前的延时时长测试按键的稳定性和手感

--- prj0/code(78Bsop32)/main.c
+++ prj0/code(78Bsop32)/main.c
@@ -6,14 +6,17 @@ void main() {
   VarsInit();
   F_ledOn();
   delayMs(500);
+  //使能看门狗
+  F_turnOnWDT();
   while (1) {
     //喂狗
-    // F_turnOnWDT();
+    F_clearWDT();
     // user coding
     // F_ledOff();
     // delayMs(500);
     // F_ledOn();
     // delayMs(500);
+    delayMs(20);
     GetKeys();
     if (D_keyValue1 == keyValue) {
       F_ledNeg();
@@ -27,7 +30,7 @@ void delayMs(uint16_t msCount) {
   for (i = 0; i < msCount; i++) {
     for (j = 0; j < 1000; j++) {
       /* code */
-      // F_turnOnWDT();
+      F_clearWDT();
     }
   }
 }
