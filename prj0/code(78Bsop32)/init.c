870f9b75b32273c987159c64cd3d1df785d2c72b
syYang <yangsyun@gmail.com>
2019-11-01 10:10:49 +0800

    初始化 T2  定时器，设置为 1ms 中断一次；

--- prj0/code(78Bsop32)/init.c
+++ prj0/code(78Bsop32)/init.c
@@ -4,7 +4,7 @@
 void SysInit() {
   F_clearWDT();
   //时钟初始化
-  CLKCON = 0x03; // Clock div 1
+  CLKCON = 0x03; // Clock div 1  7.3728Mhz
   STPPCK = 0;
   STPFCK = 0;
   _nop_();
@@ -28,6 +28,15 @@ void SysInit() {
   P1_0 = 1;
   // IO 状态初始化
   F_ledOff();
+  // T2 初始化  7.3728Mhz/2 = 3686400Hz
+  //  3686400Hz/3686.4 = 1000Hz = 1ms timer2 interrupt
+  TH2 = (65536 - 3686) / 256;
+  RCP2H = TH2;
+  TL2 = (65536 - 3686) % 256;
+  RCP2L = TL2;
+  TR2 = 1;
+  ET2 = 1;
+  EA = 1;
 }
 //=============================================================================
 void VarsInit() {}
